93bcc775fdae68248c40021323246f2eb1015694
caromm80 <caromm80@gmail.com>
2025-09-10 16:44:23 -0500

    solución taller objetos y arreglos

--- solucion/objetos.js
+++ solucion/objetos.js
@@ -1,5 +1,5 @@
 // Taller de Investigación sobre Objetos en JavaScript
-// Nombre del estudiante: [Tu nombre completo aquí]
+// Nombre del estudiante: Carolina Martinez Mesa
 
 // Parte 1: El Mundo de los Objetos
 
@@ -7,43 +7,55 @@
 // 1. Define qué es un objeto literal en JavaScript.
 /**
  * Escribe tu explicación aquí:
- * Un objeto literal es...
+ * Un objeto literal es una colecciónb de datos orednados con dos valores (llave y valor) y se escribe en singular; si veo {} (llaves) es un objeto literal.
  */
 
 // 2. Muestra la sintaxis básica para crear un objeto literal.
-// Escribe el código aquí
+// ={fruta:"mango"}
 
 // 3. Crea un objeto literal llamado `computador` con 5 propiedades.
-// Escribe el código aquí
-
+let computador ={marca:"lenovo", color:"negro", procesador:"i7", ram:"16gb", almacenamiento:"1tb"};
 
 // Muestra el objeto `computador` en la consola.
-
+console.log("Objeto Literal computador:", computador);
 
 // Investigación #2: Objeto por Constructor
 // 1. Define qué es un objeto por constructor y su diferencia con el literal.
 /**
  * Escribe tu explicación aquí:
- * Un objeto por constructor es...
+ * Un objeto por constructor: es un molde para crear varios objetos que tendran las mismas propiedades con distintos valores.
  */
 
 // 2. Crea un objeto utilizando el constructor `new Object()` y asígnale las mismas propiedades del objeto `computador`.
 // Escribe el código aquí
+let computadorConstructor = new Object();
+
 
 // Asigna las propiedades aquí
+computadorConstructor.marca = "hp";
+computadorConstructor.color = "negro";
+computadorConstructor.procesador = "i7";
+computadorConstructor.ram = "16gb";
+computadorConstructor.almacenamiento = "1tb";
 
 
 // Muestra el objeto `computadorPorConstructor` en la consola.
 
+console.log(computadorConstructor);
 
 // 3. Crea una función constructora llamada `Producto` y crea dos instancias.
+
 // Escribe la función constructora aquí
 
-
+function producto(marca, color) {
+this.marca = marca;
+this.color = color;}
 // Crea dos instancias de `Producto`
 // Escribe el código aquí
+let producto1 = new producto("totto","blanco")
+let producto2 = new producto("nike","negro")
 
 
 // Muestra las instancias en la consola
-// console.log("Instancia 1 de Producto:", producto1);
-// console.log("Instancia 2 de Producto:", producto2);
+console.log("Instancia 1 de Producto:", producto1);
+console.log("Instancia 2 de Producto:", producto2);
--- solucion/practicaUno.js
+++ solucion/practicaUno.js
@@ -7,32 +7,50 @@ const playlistInicial = [
     { titulo: "Stairway to Heaven", artista: "Led Zeppelin", duracion: 482 },
 ];
 
+console.log("Playlist inicial:", playlistInicial);
+
+
 // 1. Añadir una nueva canción al FINAL de la playlist.
 //    Utiliza un método mutable.
 //    Canción a añadir: { titulo: "Imagine", artista: "John Lennon", duracion: 183 }
-console.log("\n1. Añadir canción al final:");
 // Escribe tu código aquí
+console.log("\n1. Añadir canción al final:");
+let playlist = [
+    { titulo: "Bohemian Rhapsody", artista: "Queen", duracion: 355 },
+    { titulo: "Hotel California", artista: "Eagles", duracion: 391 },
+    { titulo: "Stairway to Heaven", artista: "Led Zeppelin", duracion: 482 },
+];
+playlist.push({ titulo: "Imagine", artista: "John Lennon", duracion: 183 });
+console.log(playlist);
 
 
 // 2. Eliminar la PRIMERA canción de la playlist.
 //    Utiliza un método mutable.
 console.log("\n2. Eliminar primera canción:");
 // Escribe tu código aquí
+playlist.shift();
+console.log(playlist);
 
 
 // 3. Crear una NUEVA playlist que contenga solo los títulos de las canciones.
 //    Utiliza un método inmutable. El arreglo original no debe cambiar.
 console.log("\n3. Crear playlist solo con títulos:");
 // Escribe tu código aquí
+const playlistTitulos = playlist.map(cancion => cancion.titulo);
+console.log(playlistTitulos);
 
 
 // 4. Crear una NUEVA playlist que contenga solo las canciones que duren más de 400 segundos.
 //    Utiliza un método inmutable.
 console.log("\n4. Crear playlist con canciones largas:");
 // Escribe tu código aquí
+const playlistLargas = playlist.filter(cancion => cancion.duracion > 400);
+console.log(playlistLargas);
 
 
 // 5. Encontrar la primera canción en la playlist que sea de "Eagles".
 //    Utiliza un método inmutable y muestra el objeto de la canción encontrada.
 console.log("\n5. Encontrar canción de Eagles:");
 // Escribe tu código aquí
+const cancionEagles = playlist.find(cancion => cancion.artista == "Eagles");
+console.log(cancionEagles);
